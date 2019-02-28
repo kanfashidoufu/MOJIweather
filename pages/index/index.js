fdf68b57d938590fb6951a162267b9d85e1002f4
王宁 <wn19950101@126.com>
2019-02-28 19:58:10 +0800

    使用color重构项目 优化项目结构

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,28 +1,57 @@
 //index.js
 //获取应用实例
-var app = getApp()
+const app = getApp()
+
 Page({
   data: {
-    motto: '欢迎使用磨叽天气',
-    more: '更多功能敬请期待',
-    ver: '1.0.9beta',
-    userInfo: {}
+    StatusBar: app.globalData.StatusBar,
+    CustomBar: app.globalData.CustomBar,
+    motto: '欢迎使用磨叽天气！',
+    version: '1.0',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
   //事件处理函数
-  bindViewTap: function() {
+  bindViewTap: function () {
     wx.navigateTo({
       url: '../logs/logs'
     })
   },
   onLoad: function () {
-    console.log('onLoad')
-    var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
-      //更新数据
-      that.setData({
-        userInfo:userInfo
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
       })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  getUserInfo: function (e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
     })
   }
 })
--- pages/weather/index/weather.js
+++ pages/weather/index/weather.js
@@ -20,9 +20,71 @@ function refreshData(that) {
 
 Page({
   data: {
+    StatusBar: app.globalData.StatusBar,
+    CustomBar: app.globalData.CustomBar,
     title: 'MOJI天气',
     weather: {},
-    backgroudUrl:''
+    backgroudUrl:'',
+    qltyMap: {
+      '优': 'bg-green',
+      '良': 'bg-yellow',
+      '轻度污染': 'bg-orange',
+      '中度污染': 'bg-red',
+      '重度污染': 'bg-mauve',
+      '严重污染': 'bg-purple',
+    },
+    contMap: {
+      '100': 'https://cdn.heweather.com/cond_icon/100.png',
+      '101': 'https://cdn.heweather.com/cond_icon/101.png',
+      '102': 'https://cdn.heweather.com/cond_icon/102.png',
+      '103': 'https://cdn.heweather.com/cond_icon/103.png',
+      '104': 'https://cdn.heweather.com/cond_icon/104.png',
+      '200': 'https://cdn.heweather.com/cond_icon/200.png',
+      '201': 'https://cdn.heweather.com/cond_icon/201.png',
+      '202': 'https://cdn.heweather.com/cond_icon/202.png',
+      '203': 'https://cdn.heweather.com/cond_icon/203.png',
+      '204': 'https://cdn.heweather.com/cond_icon/204.png',
+      '205': 'https://cdn.heweather.com/cond_icon/205.png',
+      '206': 'https://cdn.heweather.com/cond_icon/206.png',
+      '207': 'https://cdn.heweather.com/cond_icon/207.png',
+      '208': 'https://cdn.heweather.com/cond_icon/208.png',
+      '209': 'https://cdn.heweather.com/cond_icon/209.png',
+      '210': 'https://cdn.heweather.com/cond_icon/210.png',
+      '211': 'https://cdn.heweather.com/cond_icon/211.png',
+      '212': 'https://cdn.heweather.com/cond_icon/212.png',
+      '213': 'https://cdn.heweather.com/cond_icon/213.png',
+      '300': 'https://cdn.heweather.com/cond_icon/300.png',
+      '301': 'https://cdn.heweather.com/cond_icon/301.png',
+      '302': 'https://cdn.heweather.com/cond_icon/302.png',
+      '303': 'https://cdn.heweather.com/cond_icon/303.png',
+      '304': 'https://cdn.heweather.com/cond_icon/304.png',
+      '305': 'https://cdn.heweather.com/cond_icon/305.png',
+      '306': 'https://cdn.heweather.com/cond_icon/306.png',
+      '307': 'https://cdn.heweather.com/cond_icon/307.png',
+      '308': 'https://cdn.heweather.com/cond_icon/308.png',
+      '309': 'https://cdn.heweather.com/cond_icon/309.png',
+      '310': 'https://cdn.heweather.com/cond_icon/310.png',
+      '311': 'https://cdn.heweather.com/cond_icon/311.png',
+      '312': 'https://cdn.heweather.com/cond_icon/312.png',
+      '313': 'https://cdn.heweather.com/cond_icon/313.png',
+      '400': 'https://cdn.heweather.com/cond_icon/400.png',
+      '401': 'https://cdn.heweather.com/cond_icon/401.png',
+      '402': 'https://cdn.heweather.com/cond_icon/402.png',
+      '403': 'https://cdn.heweather.com/cond_icon/403.png',
+      '404': 'https://cdn.heweather.com/cond_icon/404.png',
+      '405': 'https://cdn.heweather.com/cond_icon/405.png',
+      '406': 'https://cdn.heweather.com/cond_icon/406.png',
+      '407': 'https://cdn.heweather.com/cond_icon/407.png',
+      '500': 'https://cdn.heweather.com/cond_icon/500.png',
+      '501': 'https://cdn.heweather.com/cond_icon/501.png',
+      '502': 'https://cdn.heweather.com/cond_icon/502.png',
+      '503': 'https://cdn.heweather.com/cond_icon/503.png',
+      '504': 'https://cdn.heweather.com/cond_icon/504.png',
+      '505': 'https://cdn.heweather.com/cond_icon/505.png',
+      '506': 'https://cdn.heweather.com/cond_icon/506.png',
+      '507': 'https://cdn.heweather.com/cond_icon/507.png',
+      '508': 'https://cdn.heweather.com/cond_icon/508.png'
+    }
   },
 
   bindViewTap: function () {
@@ -44,5 +106,17 @@ Page({
   onPullDownRefresh: function () {
     console.log("下拉刷新了");
     refreshData(this);
+  },
+  showModal(e) {
+    this.setData({
+      modalName: e.currentTarget.dataset.target,
+      content: e.currentTarget.dataset.content
+    })
+  },
+  hideModal(e) {
+    this.setData({
+      modalName: null,
+      content: ''
+    })
   }
 })
